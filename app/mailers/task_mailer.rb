937aff0a1e44777182c2b4629e05aa881d9b3174
Jose Espinosa <jose@revinate.com>
2014-05-19 09:25:57 -0700

    TaskMailer now just write the enviroment

--- app/mailers/task_mailer.rb
+++ app/mailers/task_mailer.rb
@@ -1,10 +1,7 @@
 class TaskMailer
   def create
-    port = Rails::Server.new.options[:Port]
     env = "#{Rails.env} "
-    if port
-      env += port
-    end
-    Task.new(:enviroment => port).save!
+    Task.new(:enviroment => env).save!
   end
+  handle_asynchronously :create
 end
